6bae8a4775a438450ab42d79f4c107ddeed969f6
Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
2023-03-03 19:14:26 +0100

    adding final code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # password_checker
 Python script that check if given password has been ever leaked or hacked before.
 It is using following website to determine password: https://haveibeenpwned.com/Passwords
+
+HOW TO USE IT:
+Run checkpassword.py through CMD and provide at least 1 additional argument (password/passwords you want to check).
+Ex: "python checkpassword.py password123 supersecret"
--- checkpassword.py
+++ checkpassword.py
@@ -1,8 +1,58 @@
 import requests
 import sys
+import hashlib
 
+def getUserInpur():
+    password = sys.argv[1:]
+    return password
 
-password = sys.argv[1:]
-url = 'https://api.pwnedpasswords.com/range/' + password[0]
-res = requests.get(url)
-print(res)
+def checkAllInputPasswords(passwordList):
+    for password in passwordList:
+        hashedPassword = hashPassword(password)
+        hashedPasswordHead = getPasswordHead(hashedPassword)
+        hashedPasswordTail = getPasswordTail(hashedPassword)
+        serverResponse = requestApiData(hashedPasswordHead)
+        passwordPwnedCount = getPasswordLeaksCount(serverResponse, hashedPasswordTail)
+        printPasswordInfo(password, passwordPwnedCount)
+
+def hashPassword(password):
+    encodedPassword = password.encode('utf-8')
+    sha1Password = hashlib.sha1(encodedPassword)
+    hashedHexPassword = sha1Password.hexdigest().upper()
+    
+    return hashedHexPassword
+
+def getPasswordHead(hashedHexPassword):
+    return hashedHexPassword[:5]
+
+def getPasswordTail(hashedHexPassword):
+    return hashedHexPassword[5:]
+
+def requestApiData(queryString):
+    try:
+        url = 'https://api.pwnedpasswords.com/range/' + queryString
+        serverResponse = requests.get(url)
+        if serverResponse.status_code != 200:
+            raise RuntimeError(f'Error fetching: {serverResponse.status_code}')
+        
+        return serverResponse
+
+    except ValueError as err:
+        print('In Fucntion ' + requestApiData.__name__ + ' expected string as an argument') 
+
+def getPasswordLeaksCount(serverHashList, referenceHashedPasswordTail):
+    hashes = (line.split(':') for line in serverHashList.text.splitlines())
+    for hashedPassword, pwnedCount in hashes:
+        if hashedPassword == referenceHashedPasswordTail:
+            return pwnedCount
+    
+    return 0
+
+def printPasswordInfo(password, pwnedCount):
+    if pwnedCount == 0:
+        print(f"{password} hasn't been leaked. It's safe to use it.")
+    else:
+        print(f"{password} has been hacked {pwnedCount} times! You shoud consider changing your password.")
+
+userInput = getUserInpur()
+checkAllInputPasswords(userInput)
